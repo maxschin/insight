5335f58aa18b2f054480c4af0b5a0341253c27d2
clemens71 <clegro99@gmx.de>
2025-03-19 01:27:58 +0100

    added opposite_of_enemy pong-rf

---
+++ reward_functions/pong/opposite_of_enemy_rf.py
@@ -0,0 +1,41 @@
+from ocatari.ram.pong import *
+
+SCORING = False
+
+
+def reward_function(self) -> float:
+    
+    global SCORING
+    reward = 0.0
+
+    game_objects = self.objects
+
+    SCREEN_WIDTH = 160
+    SCREEN_HEIGHT = 210
+        
+    # Define categories for easy identification
+    player = None
+    opponent = None
+    ball = None
+
+    # Classify objects by type
+    for obj in game_objects:
+        if isinstance(obj, Player):
+            player = obj
+        elif isinstance(obj, Enemy):
+            enemy = obj
+        elif isinstance(obj, Ball):
+            ball = obj
+
+    if player and enemy:
+        player_center = player.y + player.h / 2
+        enemy_center = enemy.y + enemy.h / 2
+        
+        mirror_player_center = SCREEN_HEIGHT - enemy_center
+        
+        difference = abs(player_center - mirror_player_center)
+        normalized_difference = difference / SCREEN_HEIGHT
+    
+        reward += 1.0 - normalized_difference
+
+    return reward
